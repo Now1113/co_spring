cef32e6fd0fc21facaf16bc17607a0dd9333dc81
now <guswp1450@naver.com>
2022-07-11 17:03:10 +0900

    first commit

---
+++ src/main/java/now/comento/conow/ConowApplication.java
@@ -0,0 +1,13 @@
+package now.comento.conow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/now/comento/conow/ConowApplicationTests.java
@@ -0,0 +1,13 @@
+package now.comento.conow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
